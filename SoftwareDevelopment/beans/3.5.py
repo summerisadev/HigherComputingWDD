17127e54f53114df53841a10bfe4c0c3d616c3a8
WhizBangPop <alexhenderson877@gmail.com>
2022-11-15 11:39:33 +0000

    iunfbvijnbvenbvijf

--- SoftwareDevelopment/beans/3.5.py
+++ SoftwareDevelopment/beans/3.5.py
@@ -1,7 +1,39 @@
 def ReadFile():
     counter = 0
-    CarRegArray = [""] * 100
-    DatesArray = [""] * 100
-    EntryTimeArray = [""] * 100
-    ExitTimeArray = [""] * 100
-tab
+    loopCounter = 0
+    CarRegArray = []
+    DatesArray = []
+    EntryTimeArray = []
+    ExitTimeArray = []
+
+    with open('SoftwareDevelopment/datafiles/Times.csv', "r") as f:
+        line = f.readline().rstrip('\n')
+        while line:
+            items = line.split(',')
+            print(f'Car "{items[0]}" Read In')
+            line = f.readline().rstrip('\n')
+
+            CarRegArray.append(items[0])
+            DatesArray.append(items[1])
+            EntryTimeArray.append(items[2])
+            ExitTimeArray.append(items[3])
+            
+            loopCounter += 1
+
+            
+
+        items = line.split(',')
+                
+        
+
+    return(CarRegArray,DatesArray,EntryTimeArray,ExitTimeArray)
+
+def CalculatePricing(CarRegArray, EntryTimeArray, ExitTimeArray):
+    print('Starting Calculations...')
+    counter = 0
+    for i in CarRegArray:
+        print(f'Car Reg: {CarRegArray[counter]}\nEntry Time: {EntryTimeArray[counter]}\n')
+        counter += 1
+
+CRA,DA,EntryTA,ExitTA = ReadFile()
+CalculatePricing(CRA, EntryTA, ExitTA)
